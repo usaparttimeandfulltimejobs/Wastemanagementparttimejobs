4e60e111eb3fdeb0056eb9b55f86b24d4900402b
AcceptGodBlessing <mdruhalaminmollah5@gmail.com>
2026-04-09 01:03:38 +0600

    Waste Management

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Redirect after 1 second
+  setTimeout(() => {
+    window.location.href = '    window.location.href = 'https://go2affm.com/c/?p=37793&o=23039&s1=WasteManagement';';
+  }, 1000);
+});
